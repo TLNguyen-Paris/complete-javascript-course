a4d5dee7265eda2f4e48c299caee58836da9bbd8
Thanh Long Nguyen <60906406+TLNguyen-Paris@users.noreply.github.com>
2020-05-20 17:23:49 +0200

    1st challenge done

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,18 @@
+// 1st coding challenge
+
+// var markHeight = prompt("What's your height ?");
+// var markWeight = prompt("And your Weight ?");
+// var markBMI = markWeight / Math.pow(markHeight,2)
+
+// var johnHeight = prompt("What's your height ?");
+// var johnWeight = prompt("And your Weight ?");
+// var johnBMI = johnWeight / Math.pow(johnHeight,2);
+
+// var compareBMI = markBMI > johnBMI;
+// console.log("Is Mark's BMI higher than John's ? "+ compareBMI);
+
+// // console.log(markHeight + " " + markWeight + " " + markBMI);
+// // console.log(johnHeight + " " + johnWeight + " " + johnBMI);
+
+
+
